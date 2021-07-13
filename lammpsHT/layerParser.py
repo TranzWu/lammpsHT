986d0ad5dd45341af8e44e7573d3c1a22025482d
Tranz Wu <tranzwu@Tranzs-MBP.fios-router.home>
2021-07-13 10:42:13 -0400

    asd

--- lammpsHT/layerParser.py
+++ lammpsHT/layerParser.py
@@ -1,7 +1,7 @@
 from lammpsHT.lineParser import Line
 import pathlib
 
-class Layer():
+class Layer(Line):
 	def __init__(self, inp, filename):
 		self.input = inp
 		self.filename = filename
@@ -15,28 +15,28 @@ class Layer():
 		clean = self.input[0].split()
 		return clean[2]
 
+	def key_value(self, k):
+		for line in self.input:
+			if k in line:
+				clean = line.split()
+				return int(clean[1])
+
 	@property
 	def N_ensemble(self):
 		if self.isFirst:
-			line = [i for i in self.input if "N_ensemble" in i]
-			clean = line[0].split()
-			return int(clean[2])
+			return self.key_value('N_ensemble')
 	
 	@property
 	def njobs(self):
-		for line in self.input:
-			if 'njobs' in line:
-				clean = line.split()
-				return int(clean[2])
+		return self.key_value('njobs')
 	
 	@property
 	def cores(self):
-		for line in self.input:
-			if 'cores' in line:
-				clean = line.split()
-				return int(clean[2])
+		return self.key_value('cores')
 
-	
+	@property
+	def use_template(self):
+		return self.key_value('use_template')
 
 	@property
 	def lines(self):
@@ -65,7 +65,8 @@ class Layer():
 					start = idx
 				if 'END pre' in line:
 					end = idx
-			return self.input[start+1: end]
+			raw = self.input[start+1: end]
+			return [self.reformat(line) for line in raw]
 	
 	@property
 	def post(self):
@@ -75,16 +76,65 @@ class Layer():
 					start = idx
 				if 'END post' in line:
 					end = idx
-			return self.input[start+1: end]
+			raw =  self.input[start+1: end]
+			return [self.reformat(line) for line in raw]
 			
-
-	def write_first_layer(self):
+	@property
+	def template_path(self):
 		path = str(pathlib.Path(__file__).parent)
-		path = f"{path}/template/parallel_template.py"
+		return f"{path}/template/"
+
+	def read_from_template(self):
+		path = f"{self.template_path}parallel_template.py"
 		with open(path, 'r') as f:
 			rad = f.readlines()
 		return rad
 
+	def write_parameters(self):
+		text = self.read_from_template()
+		for idx, line in enumerate(text):
+			if "insert parameters" in line:
+				text.insert(idx, f"njobs = {self.njobs}")
+				if self.isFirst:
+					text.insert(idx, f"cores = {self.cores}")
+					text.insert(idx, f"N_ensemble = {self.N_ensemble}")
+				if self.use_template:
+					text.insert(idx, f"template_path = {self.template_path}")
+	
+	def write_pretreatment(self):
+		for idx, line in enumerate(text):	
+			if "insert pretreatment" in line:
+				count_pre = idx
+				for p in self.pre:
+					text.insert(count_pre, p)
+					count_pre += 1
+
+	def write_code_run(self, raw):
+		text = raw[:]
+
+		for idx, line in enumerate(text):
+			if "insert code" in line:
+				count_code = idx
+				b = '    '
+				for l in self.lines:
+					for i in range(l.NParameter):
+						if self.use_template:
+							cmd = f'python {template_path}change_parameter.py '\
+								  f'--input {self.filename} '\
+								  f'--line {l.identifier} '\
+								  f'--index {l.index[i]} '\
+								  f'--new {l.parameter[i]}'\
+							  
+							cmd_wrap = f"{b}os.system('{cmd}')\n"
+							text.insert(idx, cmd_wrap)
+							count_code += 1
+		if self.isFirst:
+			lmps_cmd = f'mpirun --oversubscribe '\
+					   '-np {cores} '\
+					   f'lmp_mpi -in {self.filename} > output.lammps'\
+			lmps_wrap = f"{b}os.system(f'{lmps_cmd}')"
+			text.insert(count_code, lmps_wrap)
+		return text
 
 	def write_to_file(self):
 		return 
