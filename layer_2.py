2eda6edecff4f9e7557981c546bb1346ef9588b3
Tranz Wu <tranzwu@tranzs-mbp.wifi.local.cmu.edu>
2021-07-14 16:25:20 -0400

    asd

--- layer_2.py
+++ layer_2.py
@@ -6,9 +6,9 @@ from joblib import Parallel, delayed
 import sys
 import numpy as np
 #insert parameters here
-line_0 = [[0, 1, 2, 3, 4]]
-N_ensemble = 5
-njobs = 3
+line_0 = [[-0.1, -1.0, -1.5, -2, -2.5, -3], [-0.2, -1.0, -1.5, -2, -2.5, -3]]
+N_ensemble = 6
+njobs = 1
 #insert pretreatment
 def run(k):
     #insert preheat
@@ -16,8 +16,9 @@ def run(k):
     os.system(f'cp hole.in layer_* run_this {k}')
     os.chdir(f'{k}')
     #insert code here
-    os.system(f'change_parameter.py --input hole.in --line pressure --index 4 --new {line_0[0]}')
+    os.system(f'change_parameter.py --input hole.in --line initial_fluid --index 6 --new {line_0[1]}')
+    os.system(f'change_parameter.py --input hole.in --line initial_fluid --index 5 --new {line_0[0]}')
     #insert post-processing
-    os.system(f'python layer_2.py')
+    os.system(f'python layer_1.py')
     os.chdir('..')
 Parallel(n_jobs=njobs)(delayed(run)(i) for i in range(N_ensemble))
